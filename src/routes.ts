fa0a3676ae21d327954cfc017084d7009f7c80ce
abubnov-sf <andrii.b@scalefinal.com>
2026-06-09 17:29:31 +0200

    Add UserList component

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { User } from './components/User.tsx';
 import { NotFound } from './components/NotFound.tsx';
 import { Fallback } from './components/Fallback.tsx';
 import { ErrorComponent } from './components/ErrorComponent.tsx';
+import { UserList } from './components/UserList.tsx';
 
 export const routes = createRouter([
 	{
@@ -22,6 +23,7 @@ export const routes = createRouter([
 	},
 	{ path: '/about', element: () => import('./components/About.tsx') },
 	{ path: '/test', element: Test },
+	{ path: '/user', element: UserList },
 	{ path: '/user/:userId', element: User },
 	{
 		path: '/post/:postId',
