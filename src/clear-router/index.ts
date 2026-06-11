b09ade0482f308c0d42e1660890d90c115c3fa8b
abubnov-sf <andrii.b@scalefinal.com>
2026-06-11 16:00:06 +0200

    Export types

--- src/clear-router/index.ts
+++ src/clear-router/index.ts
@@ -9,3 +9,4 @@ export { useBeforeUnload } from './hooks/useBeforeUnload';
 export { useRouterContext } from './hooks/useRouterContext';
 export { createRouter } from './utils/utils';
 export { redirect } from './utils/redirect';
+export type { RouteItem, BlockerState } from './types/global';
--- src/clear-router/utils/redirect.ts
+++ src/clear-router/utils/redirect.ts
@@ -1,5 +1,12 @@
-export const redirect = (url: string, search?: string) => {
-	const error = new Error();
-	error.cause = 'redirect';
-	throw Object.assign(error, { url, search: search || '' });
-};
+export class Redirect {
+	url: string;
+	search?: string;
+	cause: 'redirect';
+	constructor(url: string, search?: string) {
+		this.url = url;
+		this.search = search;
+		this.cause = 'redirect';
+	}
+}
+
+export const redirect = (url: string, search?: string) => Promise.reject(new Redirect(url, search));
