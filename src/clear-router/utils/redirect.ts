457386814dc75d4db0ae42d632ef7fff4e3ab60f
abubnov-sf <andrii.b@scalefinal.com>
2026-06-11 14:11:04 +0200

    Refactor redirect

--- src/clear-router/utils/redirect.ts
+++ src/clear-router/utils/redirect.ts
@@ -1,12 +1,5 @@
-export class Redirect {
-	url: string;
-	search?: string;
-	cause: 'redirect';
-	constructor(url: string, search?: string) {
-		this.url = url;
-		this.search = search;
-		this.cause = 'redirect';
-	}
-}
-
-export const redirect = (url: string, search?: string) => Promise.reject(new Redirect(url, search));
+export const redirect = (url: string, search?: string) => {
+	const error = new Error();
+	error.cause = 'redirect';
+	throw Object.assign(error, { url, search: search || '' });
+};
