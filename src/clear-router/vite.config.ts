1d64d2d10d727d93d926a2ef14f7732199c55a5c
abubnov-sf <andrii.b@scalefinal.com>
2026-06-11 15:12:50 +0200

    Fix vite config

--- src/clear-router/vite.config.ts
+++ src/clear-router/vite.config.ts
@@ -1,18 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
 	plugins: [react()],
 
 	build: {
+		target: 'esnext',
+
 		lib: {
-			entry: 'index.ts',
+			entry: resolve(__dirname, 'index.ts'),
 			formats: ['es'],
 			fileName: () => 'index.js',
 		},
 
 		rollupOptions: {
 			external: ['react', 'react-dom'],
+
+			output: {
+				format: 'es',
+				entryFileNames: 'index.js',
+				chunkFileNames: 'chunks/[name].js',
+
+				preserveModules: false,
+			},
 		},
+
+		sourcemap: false,
+		minify: false,
 	},
 });
