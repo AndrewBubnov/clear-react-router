6043ff12eafc65d508539abc841246bb97ebe88f
abubnov-sf <andrii.b@scalefinal.com>
2026-06-13 15:51:43 +0200

    Fix naming

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Router } from './clear-router';
 import { routes } from './routes.ts';
 
 function App() {
-	return <Router routeList={routes} animated animationOptions={{ duration: 1000 }} />;
+	return <Router routeList={routes} isAnimated animationOptions={{ duration: 1000 }} />;
 }
 
 export default App;
