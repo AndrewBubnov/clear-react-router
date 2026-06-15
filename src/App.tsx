e05ff2fa44a26b238a5e7403658cf9d73e2e786a
abubnov-sf <andrii.b@scalefinal.com>
2026-06-15 16:39:03 +0200

    Remove presentation components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { Router } from './clear-router';
-import { routes } from './routes.ts';
-
 function App() {
-	return <Router routeList={routes} isAnimated animationOptions={{ duration: 500, name: 'slide-right' }} />;
+	return null;
 }
 
 export default App;
