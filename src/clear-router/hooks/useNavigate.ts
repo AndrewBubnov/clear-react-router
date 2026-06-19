40f966b700ca651c3eb7ebcff2a2cbd583bb58ef
abubnov-sf <andrii.b@scalefinal.com>
2026-06-19 15:05:56 +0200

    Create useSearchParams hook

--- src/clear-router/hooks/useNavigate.ts
+++ src/clear-router/hooks/useNavigate.ts
@@ -2,20 +2,23 @@ import { useCallback } from 'react';
 import { useRouterActions } from './useServiceContext';
 import { useLocation } from './useLocation';
 import type { Location } from '../types/global';
+import { useLatest } from './useLatest.ts';
 
 export const useNavigate = () => {
 	const { updateLocation } = useRouterActions();
 	const location = useLocation();
 
+	const locationRef = useLatest(location);
+
 	return useCallback(
 		async (arg: Location | string | number) => {
 			if (typeof arg === 'number') return history.go(arg);
 			if (typeof arg === 'string') {
-				if (arg !== location.pathname) await updateLocation({ pathname: arg });
-			} else if (JSON.stringify(arg) !== JSON.stringify(location)) {
+				if (arg !== locationRef.current.pathname) await updateLocation({ pathname: arg });
+			} else if (JSON.stringify(arg) !== JSON.stringify(locationRef.current)) {
 				await updateLocation(arg);
 			}
 		},
-		[location, updateLocation]
+		[updateLocation, locationRef]
 	);
 };
