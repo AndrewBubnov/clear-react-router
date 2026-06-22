bff94fd7eae98f9b4ed77830c479dde5d9995911
abubnov-sf <andrii.b@scalefinal.com>
2026-06-22 10:03:03 +0200

    Create useHistoricalTrail hook

---
+++ src/clear-router/hooks/useHistoricalTrail.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from './useLocation';
+
+export const useHistoricalTrail = () => {
+	const { pathname } = useLocation();
+	const [trail, setTrail] = useState<string[]>([]);
+
+	useEffect(() => {
+		if (!pathname) return;
+		// eslint-disable-next-line react-hooks/set-state-in-effect
+		setTrail(prevState => {
+			const index = prevState.indexOf(pathname);
+			return index === -1 ? [...prevState, pathname] : prevState.slice(0, index + 1);
+		});
+	}, [pathname]);
+
+	return trail;
+};
