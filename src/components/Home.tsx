05186346e2ab01f94e311a89562d8025288c48f3
abubnov-sf <andrii.b@scalefinal.com>
2026-06-23 12:24:55 +0200

    Restore Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,11 @@
-import { Link, parser } from '../clear-router';
+import { Link, useLoaderState } from '../clear-router';
 import { loremIpsum } from 'lorem-ipsum';
-import { useTypedQuery } from '../clear-router/hooks/useTypedQuery.ts';
-import { z } from 'zod';
-import { useEffect } from 'react';
-
-const schema = z.object({ name: z.string() });
 
 const Home = () => {
-	const [query, setQuery] = useTypedQuery('filter', parser.string, '');
-	useEffect(() => {
-		setQuery('Andrew');
-	}, []);
-
+	console.log(useLoaderState());
 	return (
 		<div style={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
 			<h3>Home</h3>
-			<input value={query} onChange={e => setQuery(e.target.value)} />
 			<Link to="/about">
 				<span>To about page</span>
 			</Link>
