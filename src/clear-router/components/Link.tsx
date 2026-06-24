ee1c3f0ca35594b0f4848f06440e223f1ee37595
abubnov-sf <andrii.b@scalefinal.com>
2026-06-24 09:36:02 +0200

    Add prefetch delay behaviour

--- src/clear-router/components/Link.tsx
+++ src/clear-router/components/Link.tsx
@@ -1,4 +1,4 @@
-import { type ReactElement, type MouseEvent, type CSSProperties } from 'react';
+import { type ReactElement, type MouseEvent, type CSSProperties, useRef, useCallback } from 'react';
 import { useNavigate } from '../hooks/useNavigate';
 import { useRouterActions } from '../hooks/useServiceContext';
 
@@ -6,16 +6,33 @@ type LinkProps = {
 	to: string;
 	children: ReactElement<{ onClick: (e: MouseEvent) => void; style: CSSProperties }>;
 	prefetch?: boolean;
+	prefetchDelay?: number;
 };
 
-export const Link = ({ children, to, prefetch = true }: LinkProps) => {
+const STANDARD_DELAY = 150;
+
+export const Link = ({ children, to, prefetch = true, prefetchDelay = STANDARD_DELAY }: LinkProps) => {
 	const { prefetchLoader } = useRouterActions();
 	const navigate = useNavigate();
+	const timeout = useRef<number>(0);
+
+	const onMouseEnter = useCallback(() => {
+		if (!prefetch || !prefetchDelay) return;
+		clearTimeout(timeout.current);
+		timeout.current = window.setTimeout(() => prefetchLoader(to), prefetchDelay);
+	}, [prefetch, prefetchDelay, prefetchLoader, to]);
+
+	const onMouseLeave = useCallback(() => {
+		if (!prefetch || !prefetchDelay) return;
+		clearTimeout(timeout.current);
+	}, [prefetch, prefetchDelay]);
+
 	return (
 		<a
 			style={{ cursor: 'pointer' }}
 			onClick={() => navigate(to)}
-			onMouseOver={() => prefetch && prefetchLoader(to)}
+			onMouseEnter={onMouseEnter}
+			onMouseLeave={onMouseLeave}
 		>
 			{children}
 		</a>
