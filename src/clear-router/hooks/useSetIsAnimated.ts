5ebcda42581828670fc6ace2f3edf4c13d057ab2
abubnov-sf <andrii.b@scalefinal.com>
2026-07-02 13:56:58 +0200

    Fix animation behaviour

--- src/clear-router/hooks/useSetIsAnimated.ts
+++ src/clear-router/hooks/useSetIsAnimated.ts
@@ -1,5 +1,5 @@
-import { isAnimatedStore } from '../store/isAnimatedStore.ts';
 import { useEffect } from 'react';
+import { isAnimatedStore } from '../store/isAnimatedStore';
 
 export const useSetIsAnimated = (isAnimated?: boolean) => {
 	const setIsAnimated = isAnimatedStore.use(state => state.setIsAnimated);
