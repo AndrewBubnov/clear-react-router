3ce1cabe2bc505149a302fbe54468de84e865c46
abubnov-sf <andrii.b@scalefinal.com>
2026-07-08 11:28:11 +0200

    Export RouterProps type

--- src/clear-router/index.ts
+++ src/clear-router/index.ts
@@ -14,4 +14,4 @@ export { useSearchParams } from './hooks/useSearchParams';
 export { useHistoricalTrail } from './hooks/useHistoricalTrail';
 export { adapter } from './utils/adapter.ts';
 export { createRouter } from './utils/utils';
-export type { RouteItem, BlockerState, Location, Adapter } from './types/global';
+export type { RouteItem, BlockerState, Location, Adapter, RouterProps } from './types/global';
