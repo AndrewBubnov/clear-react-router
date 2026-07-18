75aa29d1c022eaab5fcb6e766544a6370d3ac7a5
abubnov-sf <andrii.b@scalefinal.com>
2026-07-18 17:15:57 +0200

    Fix route defining

--- src/clear-router/runtime/invalidate.ts
+++ src/clear-router/runtime/invalidate.ts
@@ -6,9 +6,11 @@ import { findRoute } from '../utils/findRoute';
 import { loaderStateRef, timestampMap } from '../cell';
 import { RouteItem } from '../types/global';
 
+type Options = { withChildren?: boolean };
+
 const redirect = () => Promise.resolve();
 
-const invalidateKey = async (routeItem: RouteItem, pathname: string) => {
+const invalidatePath = async (routeItem: RouteItem, pathname: string) => {
 	const routePathname = routeItemDataState.getState().location.pathname;
 	timestampMap.delete(pathname);
 	const params = getParamsObject({ params: routeItem?.params, pathname });
@@ -25,16 +27,20 @@ const invalidateKey = async (routeItem: RouteItem, pathname: string) => {
 	if (pathname === routePathname) currentLoaderState.setState(loaderStateRef.value);
 };
 
-export const invalidateItem = async (pathname: string) => {
+const invalidateItem = async (pathname: string, withChildren?: boolean) => {
 	const routeItem = findRoute(pathname);
 	if (!routeItem) return;
 	const pathnameArray: string[] = [];
 	for (const [key] of timestampMap) if (comparePaths(routeItem, key)) pathnameArray.push(key);
-	await Promise.all(pathnameArray.map(pathname => invalidateKey(routeItem, pathname)));
+	await Promise.all(pathnameArray.map(pathname => invalidatePath(routeItem, pathname)));
+	if (withChildren && routeItem.children?.length) {
+		const childPathList = routeItem.children.map(el => el.path);
+		await Promise.all(childPathList.map(el => invalidateItem(`${pathname}${el}`, withChildren)));
+	}
 };
 
-export const invalidate = async (pathList?: string | string[]) => {
+export const invalidate = async (pathList?: string | string[], options?: Options) => {
 	const routePathname = routeItemDataState.getState().location.pathname;
 	const pathnameList = Array.isArray(pathList) ? pathList : pathList ? [pathList] : [routePathname];
-	await Promise.all(pathnameList.map(pathname => invalidateItem(pathname)));
+	await Promise.all(pathnameList.map(pathname => invalidateItem(pathname, options?.withChildren)));
 };
