d5b52c3cc9f10a4a4a6841ef2cc961b5351b54b4
abubnov-sf <andrii.b@scalefinal.com>
2026-07-22 14:15:40 +0200

    Fix not found route

--- src/clear-router/utils/findRoute.ts
+++ src/clear-router/utils/findRoute.ts
@@ -1,7 +1,7 @@
 import { comparePaths } from './utils';
 import { routerConfig } from '../config/routerConfig';
 
-export const ALL_LOCATIONS = '*';
+export const ALL_LOCATIONS = '/*';
 
 export const findRoute = (pathname: string, includeAll?: boolean) => {
 	if (includeAll) return routerConfig.routes.find(el => el.path === ALL_LOCATIONS || comparePaths(el, pathname));
