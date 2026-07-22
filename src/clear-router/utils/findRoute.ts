8b06559b966ea1e3f69f53cd0410e8aace176dfa
abubnov-sf <andrii.b@scalefinal.com>
2026-07-22 12:51:01 +0200

    Fix nested route bug

--- src/clear-router/utils/findRoute.ts
+++ src/clear-router/utils/findRoute.ts
@@ -5,5 +5,5 @@ export const ALL_LOCATIONS = '*';
 
 export const findRoute = (pathname: string, includeAll?: boolean) => {
 	if (includeAll) return routerConfig.routes.find(el => el.path === ALL_LOCATIONS || comparePaths(el, pathname));
-	return routerConfig.routes.find(el => el.path === ALL_LOCATIONS || comparePaths(el, pathname));
+	return routerConfig.routes.find(el => comparePaths(el, pathname));
 };
