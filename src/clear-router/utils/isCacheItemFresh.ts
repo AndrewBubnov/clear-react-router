4b22f89e779f573cd9e0b8f7042ec5180cf8bbf9
abubnov-sf <andrii.b@scalefinal.com>
2026-08-14 15:32:51 +0200

    Add search to path

--- src/clear-router/utils/isCacheItemFresh.ts
+++ src/clear-router/utils/isCacheItemFresh.ts
@@ -1,11 +1,10 @@
 import { routerConfig } from '../config/routerConfig';
 import { LoaderStateItem } from '../types';
 
-export const createIsCacheItemFresh =
-	(loaderMap: Map<string, LoaderStateItem>) => (pathname: string, search?: string) => {
-		const item = loaderMap.get(`${pathname}${search}`);
-		if (item === undefined) return false;
-		const resolvedStaleTime = item.staleTime ?? routerConfig.defaultStaleTime;
-		if (resolvedStaleTime === undefined) return true;
-		return Date.now() - item.timestamp <= resolvedStaleTime;
-	};
+export const createIsCacheItemFresh = (loaderMap: Map<string, LoaderStateItem>) => (path: string) => {
+	const item = loaderMap.get(path);
+	if (item === undefined) return false;
+	const resolvedStaleTime = item.staleTime ?? routerConfig.defaultStaleTime;
+	if (resolvedStaleTime === undefined) return true;
+	return Date.now() - item.timestamp <= resolvedStaleTime;
+};
