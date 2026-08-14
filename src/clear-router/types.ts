a3df0685a75a31b69c85414390f9fff825af0043
abubnov-sf <andrii.b@scalefinal.com>
2026-08-14 15:33:45 +0200

    Refactor type

--- src/clear-router/types.ts
+++ src/clear-router/types.ts
@@ -152,10 +152,7 @@ export type RouterType = {
 };
 
 export type InvalidateOptions = { withChildren?: boolean; withBeforeLoad?: boolean };
-export type RevalidateCache = ({
-	routeItem,
-	pathname,
-}: RevalidateCacheArgs) => Promise<{ data: unknown; error: unknown }>;
+export type RevalidateCache = (args: RevalidateCacheArgs) => Promise<{ data: unknown; error: unknown }>;
 export type Options =
 	| Partial<{
 			onSuccess: (args: unknown) => void;
