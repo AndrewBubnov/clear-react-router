551d756833d114160274e37089c65a6e93fa7d06
abubnov-sf <andrii.b@scalefinal.com>
2026-08-28 09:52:53 +0200

    Update Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,14 @@
-import { Link, useLoaderState } from '../clear-router';
+import { Link, useInvalidate, useLoaderState } from '../clear-router';
 import { loremIpsum } from 'lorem-ipsum';
 
 const Home = () => {
 	const { data } = useLoaderState<string>();
+	const invalidate = useInvalidate();
 	return (
-		<div style={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
+		<div style={{ display: 'flex', flexDirection: 'column', gap: 20, alignItems: 'center' }}>
 			<h3>Home</h3>
 			<span>{data}</span>
+			<button onClick={() => invalidate('/about', { force: true })}>Invalidate</button>
 			<Link to="/about">
 				<span>To about page</span>
 			</Link>
