8e3dda7a4ae836509f93c1b8390909fe1893f298
abubnov-sf <andrii.b@scalefinal.com>
2026-08-31 13:47:27 +0200

    Remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,3 @@
-import { Router } from './clear-router';
-import { routes } from './routes';
-import { Navbar } from './components/Navbar';
-
-function App() {
-	return (
-		<main>
-			<Navbar />
-			<div style={{ marginTop: 100 }}>
-				<Router routes={routes} isAnimated animationDuration={1200} />
-			</div>
-		</main>
-	);
-}
+const App = () => null;
 
 export default App;
