5da5bb00444aa3666d796e2893d927368fb66802
abubnov-sf <andrii.b@scalefinal.com>
2026-09-05 14:58:37 +0200

    Fix pushing history state

--- src/clear-router/runtime/navigate.ts
+++ src/clear-router/runtime/navigate.ts
@@ -111,7 +111,7 @@ export const createNavigate = (routerState: RouterState, revalidateCache: Revali
 		if (blockerState.getState() === 'charged') {
 			blockerState.setState('blocked');
 			blockedRouteTargetRef.set(nextLocation);
-			history.replaceState(null, '', routeItemDataState.getState().location.pathname);
+			history.pushState(null, '', routeItemDataState.getState().location.pathname);
 			return true;
 		}
 		return false;
